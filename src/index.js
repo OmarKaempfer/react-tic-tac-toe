23edd48dd78fa1542008454d175cd9619f00a837
Omar Kämpfer <omarkaempfer@gmail.com>
2019-10-24 14:11:30 +0100

    Implement turn taking logic

--- src/index.js
+++ src/index.js
@@ -2,23 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
+function Square(props) {
+  return(
+    <button className="square"
+      onClick={props.onClick}>
+        {props.value}
       </button>
-    );
-  }
+  );
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(i) {
+    if(!this.state.squares[i]) {
+      const squares = this.state.squares.slice();
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+  
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext
+      });
+    }
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+    <Square 
+      value={this.state.squares[i]}
+      onClick={() => {this.handleClick(i)}}/>
+    );
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
 
     return (
       <div>
@@ -48,7 +73,7 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board/>
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
